c1d2fd4f9d65d1d7a31438170ceb65ae33703412
Saska Dönges <tuomionpupu@gmail.com>
2021-04-27 08:31:23 +0300

    Draft of branchless binary search

--- bit_vector/simple_node.hpp
+++ bit_vector/simple_node.hpp
@@ -26,5 +26,17 @@ class simple_node {
     ~simple_node() {
         allocator_.deallocate_node(this);
     }
+
+  private:
+    uint8_t branchless(int* arr, int q) {
+        constexpr uint32_t MASK = uint32_t(1) << 31;
+        uint8_t idx = (uint8_t(1) << 5) - 1;
+        idx ^= (((arr[idx] - q) & MASK) >> 26) | (uint8_t(1) << 4);
+        idx ^= (((arr[idx] - q) & MASK) >> 27) | (uint8_t(1) << 3);
+        idx ^= (((arr[idx] - q) & MASK) >> 28) | (uint8_t(1) << 2);
+        idx ^= (((arr[idx] - q) & MASK) >> 29) | (uint8_t(1) << 1);
+        idx ^= (((arr[idx] - q) & MASK) >> 30) | uint8_t(1);
+        return idx ^ (((arr[idx] - q) & MASK) >> 31); 
+    }
 };
 #endif
