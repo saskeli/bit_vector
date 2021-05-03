8835e020447ab07c020e07eb17408b5ff1f8a6e2
Dönges Saska I <saska.dones@helsinki.fi>
2021-05-03 15:03:32 +0300

    API change in progress....

--- bit_vector/simple_node.hpp
+++ bit_vector/simple_node.hpp
@@ -6,30 +6,26 @@
 
 #define CHILDREN 32
 
-template <class data_type, class leaf_type, class allocator_type, data_type leaf_size>
+template <class data_type, class leaf_type, data_type leaf_size>
 class simple_node {
   private:
-    uint8_t meta_data_;
+    uint8_t meta_data_; // meta_data_ & 0x80 if children are leaves
     data_type child_sizes_[CHILDREN];
     data_type child_sums_[CHILDREN];
-    void* children_[CHILDREN];
-    allocator_type* allocator_;
+    void* children_[CHILDREN]; // pointers to leaf_type or simple_node elements
+    void* allocator_; //This will be the allocator
 
   public:
-    simple_node(allocator_type* allocator) {
-        meta_data_ = 0x80;
+    simple_node(void* allocator, bool has_leaves) {
+        meta_data_ = has_leavs ? 0x80 : 0x00;
         allocator_ = allocator;
         memset(&child_sizes_, 0xff, sizeof(data_type) * CHILDREN);
         memset(&child_sums_, 0xff, sizeof(data_type) * CHILDREN);
     }
     
-    ~simple_node() {
-        allocator_.deallocate_node(this);
-    }
-
   private:
-    uint8_t branchless(int* arr, int q) {
-        constexpr uint32_t MASK = uint32_t(1) << 31;
+    uint8_t find_child(data_type q) {
+        constexpr data_type MASK = data_type(1) << (sizeof(data_type) * 8 - 1);
         uint8_t idx = (uint8_t(1) << 5) - 1;
         idx ^= (((arr[idx] - q) & MASK) >> 26) | (uint8_t(1) << 4);
         idx ^= (((arr[idx] - q) & MASK) >> 27) | (uint8_t(1) << 3);
